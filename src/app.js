d51f3c52d9ef5387c7e9b141267b8749b9f3c3b9
Adam Gibbons <adam.d.gibbons@gmail.com>
2025-11-08 11:17:52 -0700

    improve clock

--- src/app.js
+++ src/app.js
@@ -69,6 +69,8 @@ function getSelectedTimeOfDay() {
   return selectedButton ? selectedButton.getAttribute('name') : 'dawn'
 }
 
+let totalMinutesElapsed = null
+
 // Clock functionality
 function updateClock() {
   const timeElement = document.getElementById('time')
@@ -90,12 +92,25 @@ function updateClock() {
   const minutesVal = targetTime.minutes
   const secondsVal = targetTime.seconds
   
-  // Hour hand: 30 degrees per hour, plus 0.5 degrees per minute
-  const hourRotation = (hours12 * 30) + (minutesVal * 0.5)
+  // Track total elapsed minutes in a forward-moving timeline (24-hour wrap)
+  const targetMinutes = (targetTime.hours * 60) + minutesVal + (secondsVal / 60)
+
+  if (totalMinutesElapsed === null) {
+    totalMinutesElapsed = targetMinutes
+  } else {
+    let minuteDelta = targetMinutes - (totalMinutesElapsed % 1440)
+    if (minuteDelta < 0) {
+      minuteDelta += 1440
+    }
+    totalMinutesElapsed += minuteDelta
+  }
+
+  // Hour hand: 0.5 degrees per minute
+  const hourRotation = totalMinutesElapsed * 0.5
   
-  // Minute hand: 6 degrees per minute, plus 0.1 degrees per second
-  const minuteRotation = (minutesVal * 6) + (secondsVal * 0.1)
-  
+  // Minute hand: 6 degrees per minute
+  const minuteRotation = totalMinutesElapsed * 6
+
   hourHand.style.transform = `rotate(${hourRotation}deg)`
   minuteHand.style.transform = `rotate(${minuteRotation}deg)`
 }
